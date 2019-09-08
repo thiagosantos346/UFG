36b5bdf713a3393ebf7a5319d42f301b58b208ad
thiagosantos346 <thiagosantos346@gmail.com>
2019-09-08 20:12:40 -0300

    recuperação de arquivos.

---
+++ SE-2019-2/Sistemas_Distribuidos/Tarefas/setembro-1/src/execice5/client.js
@@ -0,0 +1,33 @@
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+// fonte http://code.krister.ee/node-js-encode-decode-hex-and-publishing-data-to-the-bitcoin-blockchain/
+server.on(
+    'error',
+    (err)=>{
+        console.log('server error:\n$'+err.stack);
+        server.close();
+    }
+);
+
+server.on(
+    'message',
+    (msg, rinfo)=>{
+        console.log(msg);
+        var buffer = Buffer.from(msg, 'hex').toString();
+        console.log('server got:\n'+buffer+'\nfrom '+rinfo.address+':'+rinfo.port);
+    }
+);
+
+server.on(
+    'listening',
+    ()=>{
+        const address = server.address();
+        console.log('server listening: '+address.address+':'+address.port+'.');
+    }
+)
+
+server.bind({
+    address : 'localhost',
+    port : '8081',
+    exclusive : true
+});
