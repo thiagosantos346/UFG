7fcbb83cbef19b7863e17a52df1494e9f56e36c4
Luana Ferreira <luanaferreiradealmeida07@gmail.com>
2019-11-03 15:15:35 -0300

    jogo da velha criado e funcionando

---
+++ SE-2019-2/Sistemas_Distribuidos/Tarefas/Outubro-4/jogo_da_velha/src/components/default.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Default extends Component{
+
+
+    render(props){
+        return(
+            <p>{this.props.name}</p>
+        )
+    }
+    
+}
