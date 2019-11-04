d721a21ee40c73769c58498521fcbb645bb7dece
thiagosantos346 <thiagosantos346@gmail.com>
2019-11-03 22:19:54 -0300

    jogo-da-segurança pronto!

--- SE-2019-2/Sistemas_Distribuidos/Tarefas/Outubro-4/jogo_da_velha/src/App.js
+++ SE-2019-2/Sistemas_Distribuidos/Tarefas/Outubro-4/jogo_da_velha/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import Board from './components/Board';
+import BoardPicture from './components/BoardPicture';
+import BoardButton from './components/BoardButton';
 
 function App() {
   return (
+    <>
     <Board 
       squares={[
         null, null, null, 
         null, null, null, 
         null, null, null
-    ]} /> 
+    ]} />
+    <BoardPicture valuePictures={[
+                    null, null, null, 
+                    null, null, null, 
+                    null, null, null]}/>
+    <BoardButton buttons={[
+                    null, null, null, 
+                    null, null, null, 
+                    null, null, null
+                ]}/>
+    </> 
   );
 }
 
--- SE-2019-2/Sistemas_Distribuidos/Tarefas/Outubro-4/jogo_da_velha/src/stories/index.stories.js
+++ SE-2019-2/Sistemas_Distribuidos/Tarefas/Outubro-4/jogo_da_velha/src/stories/index.stories.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Default from '../components/default';
 import Square from '../components/Square';
-import SquareButton from '../components/SquareButton';
 import Board from '../components/Board';
+import SquareButton from '../components/SquareButton';
 import BoardButton from '../components/BoardButton';
+import SquarePicture from '../components/SquarePicture';
+import BoardPicture from '../components/BoardPicture';
 
 storiesOf('Default_test', module)
     .add('default', ()=> <Default name='Working'/>)
@@ -30,3 +32,13 @@ storiesOf('Default_test', module)
                             null, null, null
                         ]} 
                     />)
+    .add('squarePicture', ()=><SquarePicture
+                            valuePicture='o'
+                        />)
+    .add('boardPicture', () => <BoardPicture      
+                valuePictures={[
+                    null, null, null, 
+                    null, null, null, 
+                    null, null, null
+                ]} 
+            />)
